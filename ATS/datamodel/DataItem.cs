501aa06944d4ab8dafb63cb15fb4efc79c530bad
Alex Komarov <a.n.komarov@gmail.com>
2019-01-30 22:12:57 +0300

    Data model

---
+++ ATS/datamodel/DataItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.datamodel {
+
+    public class SpreadItem {
+        public double qty;
+        public double bid;
+        public double ask;
+    }
+
+    public class TimeInterval {
+        List<SpreadItem> items = new List<SpreadItem>();
+        public string name; // name like Hr, Q1, Q2...HH1, HH2 - or introduce enum? 
+    }
+
+    public class HourData {
+        List<SpreadItem> items = new List<SpreadItem>();
+        DateTime date;
+        SpreadItem lastItem;    // Statistic
+        SpreadItem wavg;        // Statistic
+    }
+}
